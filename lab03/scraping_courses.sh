50ab144008d46042296f8b87709ce9843c356e76
Abanob Tawfik <32289746+mariovsluigi@users.noreply.github.com>
2018-08-05 21:00:09 +1000

    Update scraping_courses.sh

--- lab03/scraping_courses.sh
+++ lab03/scraping_courses.sh
@@ -6,6 +6,7 @@ STRING_PREFIX_POSTGRAD='http://www.handbook.unsw.edu.au/vbook2018/brCoursesByAto
 if test $# -lt 1
 	then
 		echo "Usage: ./scraping_courses.sh <course 1> <course 2> <course 3>"
+		exit 1
 fi
 #began processing the course codes provided
 for course in "$@"
@@ -16,12 +17,12 @@ do
 	URL_UNDERGRAD="$STRING_PREFIX_UNDERGRAD$letter"
 	URL_POSTGRAD="$STRING_PREFIX_POSTGRAD$letter"
 	#now we want to extract and download that webpage using wget
-
 	#retrieve the html file    
 	#now we want to extract the lines with COURSE_CODE.html using egrep	
 	#now we want to remove the text sorrounding our course code using set
 	#using \\ around our course code allows us to perform our substitution
-
-	#reference 
-	wget $URL_UNDERGRAD $URL_POSTGRAD -q -O-|egrep "$1[0-9]{4}.html"|sed "s/.*\($1[0-9][0-9][0-9][0-9]\)\.html\">/\1 /"|sed "s/ [<][/][A][>][<][/][T][D][>]//"|sed "s/[<][/][A][>][<][/][T][D][>]//"|uniq|sort|uniq
+	wget $URL_POSTGRAD $URL_UNDERGRAD -q -O-|egrep "$1[0-9]{4}.html"|sed "s/.*\($1[0-9][0-9][0-9][0-9]\)\.html\">/\1 /"|sed "s/ [<][/][A][>][<][/][T][D][>]//"|sed "s/[<][/][A][>][<][/][T][D][>]//"|
+	#now we combine the data by finding the unique courses in common then sorting by course code since handbook is sorted by course code!
+	uniq|sort|uniq
 done 
+exit
