b9b9d0a7f92faef06fc7bedeae5112776bdca47b
Abanob Tawfik <32289746+mariovsluigi@users.noreply.github.com>
2018-09-17 08:58:13 +1000

    Update perl_print.pl

--- lab9/perl_print.pl
+++ lab9/perl_print.pl
@@ -3,29 +3,38 @@ use strict;
 use warnings;
 #creating main subroutine
 sub main{
+	#want to split program arguements character/character to parse in special characters
 	my @args = split//, $ARGV[0];
 	#printing the hashbang for perl
 	print "#!/usr/bin/perl -w\n";
 	#now printing strict and warnings
 	print "use strict;\n";
 	print "use warnings;\n";
-
+	#print out main sub-routine
 	print "sub main{\n";
-	print "    print \@ARGV;\n";
+	#now we want to create our array of characters
 	print "    my \@args;\n"; 
+	#for all the words in our split program array of characters
 	foreach my $word(@args){
-		print "    if($word eq \'\"\' or $word eq \'\'\'){\n";
-		print "        push \@args, \\$word;\n";
-		print "    }else{\n";
-		print "        push \@args, $word;\n";
-		print "    }";
+		#if the character is a special character
+		if($word eq '\'' or $word eq "\"" or $word eq '\\' or $word eq "\(" or $word eq "\)" or $word eq "\{" or $word eq "\}"){
+			#we want to push it into the array with escape character
+			print "    push \@args, \"\\$word\";\n";
+		}else{
+			#otherwise we just want to push into array as it is
+			print "    push \@args, \"$word\";\n";
+		}
 	}
 	print "\n";
+	#now we want to print for each character int he array
 	print "    foreach my \$word(\@args){\n";
+	#print the character
 	print "        print \"\$word\";\n";
 	print "    }\n";
+	#print a new line character at end
 	print "    print \"\\n\";\n";
 	print "}\n";
+	#call main subroutine
 	print "main();\n";
 
 }
