d2595559c00a0e9b88614513438930fdaa89b1ce
Abanob Tawfik <32289746+mariovsluigi@users.noreply.github.com>
2018-10-01 18:31:10 +1000

    Create hello_world.js

---
+++ lab10/hello_world.js
@@ -0,0 +1,9 @@
+// write a program to prepend a string passed by the function
+// with the word, with a space ie: "Hello "
+// should return the new string.
+function hello(name) {
+	//return the prepended string with a hello and space super simple
+   return "Hello " +name;
+}
+
+module.exports = hello;
