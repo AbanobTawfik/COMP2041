05d9c1c6d3b634b4a587b57e91f1809dd6ace16d
Abanob Tawfik <32289746+mariovsluigi@users.noreply.github.com>
2018-10-02 03:53:23 +1000

    Update object.js

--- lab10/object.js
+++ lab10/object.js
@@ -44,43 +44,59 @@ function Person(name, age) {
 
 // write me
 Person.prototype.buyDrink = function(drink) {
+    //if we have an alchoholic drink and the user cant drink, we return nothing
     if(drink.alcohol && !this.canDrink()){
         return;
     }
+    //if the user can spend (passed above test aswell)
     if(this.canSpend(drink.cost)){
+        //we want to add to their persons tab
         this.tab += drink.cost;
+        //now we want to make an array to store our receipts
         var arr = [];
+        //and now we want to create a new object for our receipts
         var obj = {};
+        //if this the first transaction
         if(this.historyLen == 0){
+            //we want to set the history object field to be our array of receipts
             this.history.arr = arr;
-            this.historyLen++;
+            //create our drink object based on the drink parsed in
             obj.name = drink.name;
             obj.count = 1;
             obj.total = drink.cost;
-            this.history.arr[0] = obj;
+            //set the first element in the array of recipts to be the first drink
+            this.history.arr[this.historyLen] = obj;
+            //add 1 to the length
+            this.historyLen++;
+            //return from the function
             return;
         }
-        this.historyLen++;
-
+        //if the drink has been ordered before we want to modify the 
+        //receipt for that drink index, so scan through all receipts
         for(var i = 0; i < this.history.arr.length; i++){
-            //console.log(this.history.arr[i].name + " <===> " + drink.name);
+            //if the same drink has been ordered before eg. coke and coke
             if(this.history.arr[i].name == drink.name){
+                //increment counter + total due
                 this.history.arr[i].count++;
                 this.history.arr[i].total += drink.cost;
+                //return from function, dont increment out historylength as we didn't add a new drink to our array
                 return;
             }
         }
-
+        //otherwise
+        //set the drink object parameters
         obj.name = drink.name;
         obj.count = 1;
         obj.total = drink.cost;
+        //set the next array open index, to hold the current receipt
         this.history.arr[this.historyLen] = obj;
-        this.getRecipt();
+        //increment number of drink objects length
+        this.historyLen++;
     }
 }
 // write me
 Person.prototype.getRecipt = function() {
-    
+    //return our array of receipts
     return this.history.arr;
 }
 
